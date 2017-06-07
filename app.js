c45e402b06705e19aeac221d6897a648d6633545
ChloeAcheampong <chloeacheampong@gmail.com>
2017-06-07 15:08:15 +0100

    Filters

--- app.js
+++ app.js
@@ -2,17 +2,43 @@
   'use strict';
 
   angular.module('DIApp', [])
-  .controller('DIController', DIController);
-  DIController.$inject = ['$scope','$filter'];
-  function DIController($scope, $filter) {
+  .controller('DIController', DIController)
+  .filter('hates', hatesFilter)
+  .filter('truth', TruthFilter);
+  DIController.$inject = ['$scope','hatesFilter'];
+  function DIController($scope, hatesFilter ) {
      $scope.stateOfBeing="happy";
-    //  $scope.upper = function () {
-    //    var upCase = $filter('uppercase');
-    //    $scope.name = upCase($scope.name);
-    //  }
+     $scope.message="I love you!";
      $scope.showStateOfBeing = function () {
        $scope.stateOfBeing = "sad";
      }
+
+     $scope.sayMessage = function () {
+      return $scope.message;
+    };
+
+    $scope.sayHateMessage = function () {
+    var msg = hatesFilter($scope.message)
+     return msg;
+   };
+
+
+  }
+
+  function hatesFilter() {
+    return function (input) {
+      input = input || "";
+      input = input.replace("love","hate");
+      return input;
+    };
+  }
+
+  function TruthFilter(){
+    return function (input, target, word) {
+      input = input || "";
+      input = input.replace(target,word);
+      return input;
+    }
 
   }
 
